f63f64684e95af1f14ea2acd565ad52faaec8dad
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2026-07-19 16:29:36 +1000

    Bug Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -67,8 +67,8 @@ int main(int argc, char *argv[])
 		inRange(hsv_img, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), thresh_img);
 
 		// Apply morphology to image
-		morphologyEx(thresh_img, thresh_img, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3,3)))
-		morphologyEx(thresh_img, thresh_img, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3,3)))
+		morphologyEx(thresh_img, thresh_img, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3,3)));
+		morphologyEx(thresh_img, thresh_img, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3,3)));
 
 		// Show the thresholded image
 		imshow("Thresholded", thresh_img);
